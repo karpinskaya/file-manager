f8d9581b27bdef209d864f30fff8e8083cc51bd9
karpinskaya <kivvvimail@gmail.com>
2022-12-07 17:22:16 +0300

    feat: add constants.js

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+export const INVALID_INPUT_MSG = 'Invalid input';
+export const OPERATION_FAILED_MSG = 'Operation failed';
+
+export const COMMANDS_WITHOUT_PARAMS = ['up', 'ls'];
+export const COMMANDS_WITH_PARAMS = ['cd', 'cat', 'add', 'rn', 'cp', 'mv', 'rm', 'os', 'hash', 'compress', 'decompress'];
+export const OS_PARAMS = ['--EOL', '--cpus', '--homedir', '--username', '--architecture'];
